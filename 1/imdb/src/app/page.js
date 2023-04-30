4142b1e0e95f2620ebbf6743873100e294edadaf
paudel54 <2sansrit@gmail.com>
2023-04-30 14:00:02 +0545

    Created and added vs code extension

--- 1/imdb/src/app/page.js
+++ 1/imdb/src/app/page.js
@@ -3,7 +3,9 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <div className='bg-red-700 p-5'>
-      Hello people it's <h1 className='text-white'>HOME</h1>
+      Hello people it's
+      <h1 className='text-white'>HOME</h1>
     </div>
   )
 }
+
