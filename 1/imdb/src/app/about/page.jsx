ca352e514a549d2fe0026582669b78f16fa0e860
paudel54 <2sansrit@gmail.com>
2023-05-01 15:41:14 +0545

    Added Navbar with dynamic , useSearchParams hook

--- 1/imdb/src/app/about/page.jsx
+++ 1/imdb/src/app/about/page.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const About = () => {
   return (
-    <div className="max-w-6xl mx-auto space-y-4">
+    <div className="max-w-6xl mx-auto space-y-4 text-justify p-4">
       <h1 className="text-2xl font-medium text-amber-600">About</h1>
       <p>
         Welcome to our movie review website, where we provide insightful and
