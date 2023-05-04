e2a9c554d81b383af42276e1b9738a2ce83ee8bf
paudel54 <2sansrit@gmail.com>
2023-05-04 07:33:25 +0545

    Created header sectoin along with solving bug with babel/rc error

--- 2/google/src/app/page.jsx
+++ 2/google/src/app/page.jsx
@@ -1,7 +1,12 @@
+import HomeHeader from "@/components/HomeHeader";
 import React from "react";
 
-const page = () => {
-  return <div>This is home guys</div>;
+const Home = () => {
+  return (
+    <div>
+      <HomeHeader />
+    </div>
+  );
 };
 
-export default page;
+export default Home;
---
+++ 2/google/src/components/HomeHeader.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import Link from "next/link";
+import { BsFillGrid3X3GapFill } from "react-icons/bs";
+const HomeHeader = () => {
+  return (
+    <div>
+      <header className="">
+        <div className="flex justify-end items-center p-5 text-sm space-x-4">
+          <Link className="hover:underline" href={"https://mail.google.com"}>
+            Gmail
+          </Link>
+          <Link className="hover:underline" href={"https://image.google.com"}>
+            Images
+          </Link>
+          <BsFillGrid3X3GapFill className=" text-4xl p-2 bg-transparent hover:bg-gray-300 hover:rounded-full" />
+          <button className="bg-blue-500 text-white px-6 py-2 font-medium hover:brightness-105 hover:shadow-md transition-shadow">
+            Sign in
+          </button>
+        </div>
+      </header>
+    </div>
+  );
+};
+
+export default HomeHeader;
