c5baad5ab4f3cf90d5267f0a4e166c69e0ab6005
paudel54 <2sansrit@gmail.com>
2023-05-07 18:08:23 +0545

    Added Paginations

--- 2/google/src/app/search/image/page.jsx
+++ 2/google/src/app/search/image/page.jsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 import ImageSearchResults from "@/components/ImageSearchResults";
 export default async function ImageSearchPage({ searchParams }) {
   // to solve API exhaust:
-  await new Promise((resolve) => setTimeout(resolve, 3000));
+  await new Promise((resolve) => setTimeout(resolve, 1000));
   const response = await fetch(
     `https://www.googleapis.com/customsearch/v1?key=${process.env.GOOGLE_API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}&searchType=image`
   );
--- 2/google/src/components/ImageSearchResults.jsx
+++ 2/google/src/components/ImageSearchResults.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import Link from "next/link";
+import PaginationButtons from "./PaginationButtons";
 export default function ImageSearchResults({ results }) {
   //   console.log(results);
   return (
-    <div className="pb-24 mt-4">
+    <div className="sm:pb-24 pb-40">
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 px-3 space-y-4 space-x-4">
         {results.items.map((result) => (
           <div key={result.link} className="mb-8">
@@ -30,6 +31,9 @@ export default function ImageSearchResults({ results }) {
             </div>
           </div>
         ))}
+      </div>
+      <div className="ml-16">
+        <PaginationButtons />
       </div>
     </div>
   );
