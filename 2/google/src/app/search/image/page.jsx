7f0b3fb3a7545ddd3cfdd0f9f8edb5651cabc8df
paudel54 <2sansrit@gmail.com>
2023-05-07 16:29:35 +0545

    added image results on cards

--- 2/google/src/app/search/image/page.jsx
+++ 2/google/src/app/search/image/page.jsx
@@ -1,5 +1,32 @@
 import React from "react";
+import Link from "next/link";
+import ImageSearchResults from "@/components/ImageSearchResults";
+export default async function ImageSearchPage({ searchParams }) {
+  // to solve API exhaust:
+  await new Promise((resolve) => setTimeout(resolve, 3000));
+  const response = await fetch(
+    `https://www.googleapis.com/customsearch/v1?key=${process.env.GOOGLE_API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}&searchType=image`
+  );
 
-export default function ImageSearchPage() {
-  return <div>Image Search Page</div>;
+  // if (!response.ok) {
+  //   throw new Error("Something Went Wrong");
+  // }
+  const data = await response.json();
+  const results = data.items;
+  // console.log(results);
+  if (!results) {
+    return (
+      <div className="flex flex-col justify-center items-center pt-10">
+        <h1 className="text-3xl mb-4 ">No Results Found!</h1>
+        <p className="text-lg">
+          Try Searching for Something Else of go back to homepage.
+        </p>
+        <Link href="/" className="text-blue-500">
+          Home
+        </Link>
+      </div>
+    );
+  }
+
+  return <div>{results && <ImageSearchResults results={data} />}</div>;
 }
---
+++ 2/google/src/components/ImageSearchResults.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import Link from "next/link";
+export default function ImageSearchResults({ results }) {
+  //   console.log(results);
+  return (
+    <div className="pb-24 mt-4">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 px-3 space-y-4 space-x-4">
+        {results.items.map((result) => (
+          <div key={result.link} className="mb-8">
+            <div className="group">
+              <div>
+                <Link href={result.image.contextLink}>
+                  <img
+                    src={result.link}
+                    alt={result.title}
+                    className="h-60 group-hover:shadow-xl w-full object-contain transition-shadow"
+                  />
+                </Link>
+                <Link href={result.image.contextLink}>
+                  <h2 className="group-hover:underline truncate text-xl">
+                    {result.title}
+                  </h2>
+                </Link>
+                <Link href={result.image.contextLink}>
+                  <p className="group-hover:underline truncate text-gray-600">
+                    {result.displayLink}
+                  </p>
+                </Link>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
