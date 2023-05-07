33d020e4512ce7bab4a7064ca06560147bd4e734
paudel54 <2sansrit@gmail.com>
2023-05-07 15:18:10 +0545

    Created Search Result componnets

--- 2/google/src/app/search/web/page.jsx
+++ 2/google/src/app/search/web/page.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import Link from "next/link";
+import WebSearchResults from "@/components/WebSearchResults";
 export default async function WebSearchPage({ searchParams }) {
+  // to solve API exhaust:
+  await new Promise((resolve) => setTimeout(resolve, 10000));
   const response = await fetch(
     `https://www.googleapis.com/customsearch/v1?key=${process.env.GOOGLE_API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}`
   );
@@ -25,7 +28,5 @@ export default async function WebSearchPage({ searchParams }) {
     );
   }
 
-  return (
-    <div>{results && results.map((result) => <h1>{result.title}</h1>)}</div>
-  );
+  return <div>{results && <WebSearchResults results={data} />}</div>;
 }
