3b7e61caa3256a2f3f318dc140f5eca5c4eee610
paudel54 <2sansrit@gmail.com>
2023-05-07 12:17:32 +0545

    fetch data from google search API

--- 2/google/src/app/search/web/page.jsx
+++ 2/google/src/app/search/web/page.jsx
@@ -1,5 +1,13 @@
 import React from "react";
 
-export default function WebSearchPage() {
-  return <div>Web Search pAge is here</div>;
+export default async function WebSearchPage({ searchParams }) {
+  const response = await fetch(
+    `https://www.googleapis.com/customsearch/v1?key=${process.env.GOOGLE_API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}`
+  );
+  const data = await response.json();
+  const results = data.items;
+  console.log(results);
+  return (
+    <div>{results && results.map((result) => <h1>{result.title}</h1>)}</div>
+  );
 }
