c6de2a43ab2d619292f014b7f19529e99ee5dbf3
paudel54 <2sansrit@gmail.com>
2023-05-16 16:31:02 +0545

    Added the menu and profile image at the right side of the header

--- ig/src/app/page.js
+++ ig/src/app/page.js
@@ -4,7 +4,7 @@ import Header from "@/components/Header";
 export default function Home() {
   return (
     <div>
-      Welcome to next Js project
+
       {/* Header */}
       <Header />
       {/* Feed Section */}
--- ig/src/components/Header.js
+++ ig/src/components/Header.js
@@ -1,18 +1,27 @@
 // Es7 redux redux
 import React from 'react';
 import Image from 'next/image';
-import { BsSearch } from 'react-icons/bs';
+import { BsSearch, BsFillPlusCircleFill } from 'react-icons/bs';
+import { ImHome } from 'react-icons/im';
+
 export default function Header() {
     return (
         <div className=''>
             {/* left */}
-            <div className='flex items-center justify-between max-w-6xl'>
-                <div className='cursor-pointer h-24 w-24 relative lg:inline-grid'>
+            <div className='flex items-center justify-between max-w-6xl mx-4 xl:mx-auto'>
+                <div className='cursor-pointer h-24 w-24 relative hidden lg:inline-grid'>
                     <Image src="https://logos-download.com/wp-content/uploads/2016/03/Instagram_Logo_2016.png"
                         alt="Instagram Logo"
                         className='object-contain'
                         fill={true}
 
+                    />
+                </div>
+                <div className='cursor-pointer h-24 w-10 relative lg:hidden'>
+                    <Image src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Instagram_logo_2016.svg/2048px-Instagram_logo_2016.svg.png"
+                        alt="Instagram Logo"
+                        className='object-contain'
+                        fill={true}
                     />
                 </div>
 
@@ -24,19 +33,20 @@ export default function Header() {
                     <input type='text' placeholder='Search' className='bg-gray-50 pl-10 border-gray-500 text-sm focus:ring-black focus:border-black rounded-md ' />
                 </div>
 
-                <div className='cursor-pointer h-24 w-10 relative lg:inline-grid'>
-                    <Image src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Instagram_logo_2016.svg/2048px-Instagram_logo_2016.svg.png"
-                        alt="Instagram Logo"
-                        className='object-contain'
-                        fill={true}
-                    />
+                {/* right */}
+                <div>
+                    <div className='flex space-x-4 items-center'>
+                        <ImHome className="hidden md:inline-flex h-6 cursor-pointer hover:scale-125 transition-transform duration-200 ease-out" />
+                        <BsFillPlusCircleFill className='h-6 cursor-pointer hover:scale-125 transition-transform duration-200 ease-out' />
+                        <img src='https://xsgames.co/randomusers/avatar.php?g=male'
+                            alt='user image' className='h-10 rounded-full' />
+                    </div>
                 </div>
 
             </div>
 
 
-            {/* right */}
-            <h1>Right Side</h1>
+
         </div>
     )
 }
