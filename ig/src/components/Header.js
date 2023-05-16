cf3c53981c3ad7b3c07bfb2c9067e77d1378647f
paudel54 <2sansrit@gmail.com>
2023-05-16 13:15:19 +0545

    Added Search input bar to header component

--- ig/src/components/Header.js
+++ ig/src/components/Header.js
@@ -1,6 +1,7 @@
 // Es7 redux redux
 import React from 'react';
 import Image from 'next/image';
+import { BsSearch } from 'react-icons/bs';
 export default function Header() {
     return (
         <div className=''>
@@ -14,18 +15,28 @@ export default function Header() {
 
                     />
                 </div>
+
+                {/* mid */}
+                <div className='relative mt-1'>
+                    <div className='absolute top-2 left-2'>
+                        <BsSearch className='h-5 text-gray-500' />
+                    </div>
+                    <input type='text' placeholder='Search' className='bg-gray-50 pl-10 border-gray-500 text-sm focus:ring-black focus:border-black rounded-md ' />
+                </div>
+
                 <div className='cursor-pointer h-24 w-10 relative lg:inline-grid'>
                     <Image src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Instagram_logo_2016.svg/2048px-Instagram_logo_2016.svg.png"
                         alt="Instagram Logo"
                         className='object-contain'
                         fill={true}
-
                     />
                 </div>
-                <h1>Right Side</h1>
+
             </div>
-            {/* mid */}
+
+
             {/* right */}
+            <h1>Right Side</h1>
         </div>
     )
 }
