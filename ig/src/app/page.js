bda577b470c2ef40484b4f4dc004140996e21129
paudel54 <2sansrit@gmail.com>
2023-05-16 07:26:46 +0545

    next js instagram clone init

---
+++ ig/src/app/page.js
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <div>
+      Welcome to next Js project
+      {/* Header */}
+      {/* Feed Section */}
+      {/* Modal Section */}
+    </div>
+
+  )
+}
