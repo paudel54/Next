41a089ce8fe89523b1f9102d3f8954380d6f0031
paudel54 <2sansrit@gmail.com>
2023-05-17 08:00:25 +0545

    made the feed section responsive

--- ig/src/components/Feed.js
+++ ig/src/components/Feed.js
@@ -3,14 +3,14 @@ import Stories from './Stories'
 import Posts from './Posts'
 export default function Feed() {
     return (
-        <main>
-            <section>
+        <main className='grid grid-cols-1 md:grid-cols-3 md:max-w-6xl mx-auto'>
+            <section className='md:col-span-2'>
                 {/* Stories */}
                 <Stories />
                 {/* Posts */}
                 <Posts />
             </section>
-            <section>
+            <section className='hidden md:col-span-1 md:inline-grid'>
                 {/* Mini Profile */}
 
                 {/* Suggestions */}
