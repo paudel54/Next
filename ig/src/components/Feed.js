1b3f5a1467c2cdc7e3100ff1a5e063c1e210087b
paudel54 <2sansrit@gmail.com>
2023-05-17 11:42:31 +0545

    Create the mini profile component

--- ig/src/components/Feed.js
+++ ig/src/components/Feed.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Stories from './Stories'
 import Posts from './Posts'
+import MiniProfile from './MiniProfile'
 export default function Feed() {
     return (
         <main className='grid grid-cols-1 md:grid-cols-3 md:max-w-6xl mx-auto'>
@@ -11,10 +12,13 @@ export default function Feed() {
                 <Posts />
             </section>
             <section className='hidden md:col-span-1 md:inline-grid'>
-                {/* Mini Profile */}
 
-                {/* Suggestions */}
+                <div className='fixed w-[380px]'>
+                    {/* Mini Profile */}
+                    <MiniProfile />
+                    {/* Suggestions */}
+                </div>
             </section>
-        </main>
+        </main>//
     )
 }
---
+++ ig/src/components/MiniProfile.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function MiniProfile() {
+    return (
+        <div className='flex items-center justify-between'>
+            <img className='h-16 rounded-full border p-[2px]' src='https://i.pravatar.cc/150?img=20' alt='logo' />
+            <div className='flex-1 ml-4'>
+                <h2 className='font-bold'>Anna Taylor</h2>
+                <h3 className='text-sm text-gray-400'>Welcome to instagram</h3>
+            </div>
+            <button className='font-semibold text-blue-400 text-sm'>Sign Out</button>
+        </div>
+    )
+}
