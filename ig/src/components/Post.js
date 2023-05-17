49503a9410b414b21e3f3cd66dacdaa6fecbcb0e
paudel54 <2sansrit@gmail.com>
2023-05-17 06:58:11 +0545

    Created header and image section of post Components

--- ig/src/components/Post.js
+++ ig/src/components/Post.js
@@ -1,9 +1,16 @@
 import React from 'react'
-
+import { BiDotsHorizontalRounded } from 'react-icons/bi'
 export default function Post({ id, username, userImg, img, caption }) {
     return (
-        <div>
-            {username}
+        <div className='bg-white my-7 border rounded-md'>
+            {/* Post Header */}
+            <div className='flex items-center p-5'>
+                <img className='h-12 rounded-full object-cover border p-1 mr-3' src={userImg} alt={username} />
+                <p className='font-bold flex-1'>{username}</p>
+                <BiDotsHorizontalRounded className='h-5' />
+            </div>
+            {/* Post Image */}
+            <img src={img} className='object-cover w-full' />
         </div>
     )
 }
