cd0b4ac1fa42f40dc78bf14667dae3d5d741c7f0
paudel54 <2sansrit@gmail.com>
2023-05-17 13:09:46 +0545

    Installed next-Auth and firebase and initialized the firebase

---
+++ ig/firebase.js
@@ -0,0 +1,25 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp, getApp, getApps } from "firebase/app";
+import { getFirestore } from 'firebase/firestore';
+import { getStorage } from 'firebase/storage';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.PROJECT_ID,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGE_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID
+};
+
+// Initialize Firebase
+// if no app initailize , if there exists an app get App
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+// intitialize firestore db
+const db = getFirestore();
+const storage = getStorage();
+
+export { app, db, storage }
